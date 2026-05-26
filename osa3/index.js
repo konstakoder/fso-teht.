03e39ea00fdf9ccb58c072eb544050dae451477c
konsta <ktkors@utu.fi>
2026-05-26 18:30:31 +0300

    osa 3 a, teht. 3.1-3.8

---
+++ osa3/index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+const morgan = require('morgan')
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+  { 
+    id: "1",
+    name: "Arto Hellas", 
+    number: "040-123456"
+  },
+  { 
+    id: "2",
+    name: "Ada Lovelace", 
+    number: "39-44-5323523"
+  },
+  { 
+    id: "3",
+    name: "Dan Abramov", 
+    number: "12-43-234345"
+  },
+  { 
+    id: "4",
+    name: "Mary Poppendieck", 
+    number: "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+  const count = persons.length
+  const date = new Date()
+  
+  res.send(`
+    <p>Phonebook has info for ${count} people</p>
+    <p>${date}</p>
+  `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const person = persons.find(p => p.id === req.params.id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).json({ error: 'person not found' })
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  persons = persons.filter(p => p.id !== req.params.id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body
+
+  if (!name || !number) {
+    return res.status(400).json({ error: 'name or number missing' })
+  }
+
+  if (persons.find(p => p.name === name)) {
+    return res.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    id: String(Math.floor(Math.random() * 1000000)),
+    name,
+    number
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+morgan.token('body', (req) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+  return ''
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})

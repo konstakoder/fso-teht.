91be122f30fe3f808371e7f567dd802c10d5488b
konsta <ktkors@utu.fi>
2026-05-26 17:06:04 +0300

    2.16-2.17 osa 2 e

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PersonForm from './personForm'
-import axios from 'axios'
-import { useEffect } from 'react'
-import getAll from './post'
-import create from './post'
-import update from './post'
-import remove from './post'
+// import update from './post'
+// import getAll from './post'
+// import create from './post'
+// import remove from './post'
+import Notification from './Notification'
+import './index.css'
+import * as postService from './post'
 
 
 
@@ -16,9 +17,13 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
+  const [addMessage, setAddMessage] = useState(null)
+  const [messageType, setMessageType] = useState('success')
+
+
 
   useEffect(() => {
-    getAll.getAll()
+    postService.getAll()
       .then(initialPersons => {
         setPersons(initialPersons)
       })
@@ -28,9 +33,16 @@ const App = () => {
   const deletePerson = (id) => {
     const person = persons.find(p => p.id === id)
     if (window.confirm(`Delete ${person.name}?`)) {
-      remove.remove(id)
+      postService.remove(id)
         .then(() => {
           setPersons(persons.filter(p => p.id !== id))
+
+          setMessageType('success')
+          setAddMessage(`Deleted ${person.name}`)
+          setTimeout(() => {
+            setAddMessage(null)
+          }, 5000);
+
         })
     }
   }
@@ -46,39 +58,58 @@ const App = () => {
 
   if (existingPerson) {
     if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
-      update.update(existingPerson.id, personObject)
+      postService.update(existingPerson.id, personObject)
         .then(returnedPerson => {
           setPersons(persons.map(p => p.id !== existingPerson.id ? p : returnedPerson))
+          setMessageType('success')
+          setAddMessage(`Updated ${newName}'s number`)
+          setTimeout(() => {
+            setAddMessage(null)
+          }, 5000);
           setNewName('')
           setNewNumber('')
+        })
+        .catch(error => {
+          setMessageType('error')
+          setAddMessage(`Information of ${newName} has already been removed from server`)
+          setTimeout(() => {
+            setAddMessage(null)
+          }, 5000);
         })
     }
     return
   }
+  
 
     // setPersons(persons.concat(personObject))
-    create.create(personObject)
+    postService.create(personObject)
       .then(returnedPerson => {
         setPersons(persons.concat(returnedPerson))
+        setMessageType('success')
+        setAddMessage(`Added ${newName}`)
+        setNewName('')
+        setNewNumber('')
+        setTimeout(() => {
+          setAddMessage(null)
+        }, 5000);
       })
-    setNewName('')
-    setNewNumber('')
+      .catch(error => {
+        setMessageType('error')
+        setAddMessage('Failed to add person')
+        setTimeout(() => {
+          setAddMessage(null)
+        }, 5000);
+      })
   }
 
 
+  const [numbers, setNumbers] = useState([
+  { number: '' }
+  ])
 
+  const [searchName, setSearchName] = useState('')
 
-
-
-
-
-const [numbers, setNumbers] = useState([
- { number: '' }
-])
-
-const [searchName, setSearchName] = useState('')
-
-const filteredPersons = persons.filter(person => person.name && person.name.toLowerCase().includes(searchName.toLowerCase()))
+  const filteredPersons = persons.filter(person => person.name && person.name.toLowerCase().includes(searchName.toLowerCase()))
 
 
 
@@ -89,6 +120,7 @@ const filteredPersons = persons.filter(person => person.name && person.name.toLo
 
       <h2>Add a new</h2>
       <PersonForm addPerson={addPerson} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
+      <Notification message={addMessage} type={messageType} />
       <h2>Numbers</h2>
       <ul>
         {filteredPersons.map(person => <li key={person.name}>{person.name} {person.number} {''} <button onClick={() => deletePerson(person.id)}>Delete</button></li>)}
